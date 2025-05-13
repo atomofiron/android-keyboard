765543ae7e917f08227f9eb358bc2f43dbdec1fd
atomofiron <atomofiron@gmail.com>
2025-05-13 05:12:17 +0200

    up sdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "demo.atomofiron.keyboard"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "demo.atomofiron.keyboard"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
